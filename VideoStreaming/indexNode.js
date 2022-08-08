e658bb08b7a01225bb20037c666ab6a2b36ba8e7
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-08-08 23:48:47 +0530

    added code to send video in chunk as per the requested range

--- VideoStreaming/indexNode.js
+++ VideoStreaming/indexNode.js
@@ -13,12 +13,30 @@ server.on('request', (req, res) => {
     }
     if (req.url == '/vid.mp4')
     {
-        console.log( "in url : '/vid'" );
+        console.log("in url : '/vid'");
         
-        var myVideoStream = fs.createReadStream(__dirname + '/vid.mp4');
+        const range = req.headers.range;
+        console.log(range);
+        const videoSize = fs.statSync('./vid.mp4').size;
+        const CHUNK_SIZE = 10 ** 6; //1MB
+        const start = Number((range.match(/\d+/))[0]);
+        console.log(start);
+        const end = Math.min(start + CHUNK_SIZE, videoSize-1)
+        const contentLength = end - start+1;
+
+
+        res.writeHead(206, {
+                "Content-Range":`bytes ${start}-${end}/${videoSize}`,
+                "Accept-Ranges": "bytes",
+                "Content-Length": contentLength,
+                "Content-Type": "video/mp4"  
+            });
+        
+        var myVideoStream = fs.createReadStream(__dirname + '/vid.mp4',{start,end});
 
         myVideoStream.on('data', (chunk) => {
             console.log("new video part received");
+            
             res.write(chunk, () => {
                 console.log( "sent video part" );    
             }) 
