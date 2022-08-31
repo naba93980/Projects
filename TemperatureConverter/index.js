69556cafd0078c77957c6f20c9ddba2558ee9f80
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-08-31 17:31:20 +0530

    added logic to get the entered values and convert

---
+++ TemperatureConverter/index.js
@@ -0,0 +1,22 @@
+const form = document.getElementById('form');
+const tempUnit = document.getElementById('unit');
+const temperature = document.getElementById('temperature');
+const result = document.getElementById("result");
+function toFarenheit(celsius) {
+    return ((9 * celsius) / 5) + 32;
+}
+
+function toCelsius(farenheight) {
+    return ((5 / 9) * (farenheight - 32));
+}
+
+form.addEventListener('submit', (e) => {
+    const unit = tempUnit.options[tempUnit.selectedIndex].value;
+    if (unit == "celsius")
+        result.innerText = `The temperature in farenheight ${toFarenheit(temperature.value)}`;
+    else {
+        if (unit == "farenheight")
+        result.innerText = `The temperature in celsius ${toCelsius(temperature.value)}`;
+    }
+    e.preventDefault();
+})
